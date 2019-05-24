946cc80075bffe1b02ff69d0c8bf76b844a13a3d
yusuf hammadi <ucup1745@gmail.com>
2019-05-24 20:07:24 +0700

    yh

---
+++ TAucup/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace TAucup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cbmobil = new System.Windows.Forms.ComboBox();
+            this.twpinjam = new System.Windows.Forms.DateTimePicker();
+            this.tmpinjam = new System.Windows.Forms.DateTimePicker();
+            this.twkembali = new System.Windows.Forms.DateTimePicker();
+            this.tmkembali = new System.Windows.Forms.DateTimePicker();
+            this.txtbiaya = new System.Windows.Forms.TextBox();
+            this.txtdurasi = new System.Windows.Forms.TextBox();
+            this.txtbayar = new System.Windows.Forms.TextBox();
+            this.btnhitung = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RENTAL MOBIL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Pilihan Mobil";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 124);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tanggal Pinjam";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 161);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tanggal Kembali";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 196);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Biaya per Jam";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(445, 124);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Pukul";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(445, 161);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Pukul";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(275, 298);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 20);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Total Bayar";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(445, 196);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(150, 20);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Total Durasi (Jam)";
+            // 
+            // cbmobil
+            // 
+            this.cbmobil.Items.AddRange(new object[] {
+            "Avanza",
+            "Xenia",
+            "APV",
+            "Innova",
+            "Alphard"});
+            this.cbmobil.Location = new System.Drawing.Point(217, 89);
+            this.cbmobil.Margin = new System.Windows.Forms.Padding(4);
+            this.cbmobil.Name = "cbmobil";
+            this.cbmobil.Size = new System.Drawing.Size(203, 24);
+            this.cbmobil.TabIndex = 0;
+            this.cbmobil.SelectedIndexChanged += new System.EventHandler(this.cbmobil_SelectedIndexChanged);
+            // 
+            // twpinjam
+            // 
+            this.twpinjam.CustomFormat = "HH:mm";
+            this.twpinjam.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.twpinjam.Location = new System.Drawing.Point(617, 124);
+            this.twpinjam.Margin = new System.Windows.Forms.Padding(4);
+            this.twpinjam.Name = "twpinjam";
+            this.twpinjam.ShowUpDown = true;
+            this.twpinjam.Size = new System.Drawing.Size(99, 22);
+            this.twpinjam.TabIndex = 3;
+            // 
+            // tmpinjam
+            // 
+            this.tmpinjam.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.tmpinjam.Location = new System.Drawing.Point(217, 124);
+            this.tmpinjam.Margin = new System.Windows.Forms.Padding(4);
+            this.tmpinjam.Name = "tmpinjam";
+            this.tmpinjam.Size = new System.Drawing.Size(203, 22);
+            this.tmpinjam.TabIndex = 1;
+            this.tmpinjam.ValueChanged += new System.EventHandler(this.tmpinjam_ValueChanged);
+            // 
+            // twkembali
+            // 
+            this.twkembali.CustomFormat = "HH:mm";
+            this.twkembali.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.twkembali.Location = new System.Drawing.Point(617, 161);
+            this.twkembali.Margin = new System.Windows.Forms.Padding(4);
+            this.twkembali.Name = "twkembali";
+            this.twkembali.ShowUpDown = true;
+            this.twkembali.Size = new System.Drawing.Size(99, 22);
+            this.twkembali.TabIndex = 4;
+            // 
+            // tmkembali
+            // 
+            this.tmkembali.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.tmkembali.Location = new System.Drawing.Point(217, 156);
+            this.tmkembali.Margin = new System.Windows.Forms.Padding(4);
+            this.tmkembali.Name = "tmkembali";
+            this.tmkembali.Size = new System.Drawing.Size(203, 22);
+            this.tmkembali.TabIndex = 2;
+            this.tmkembali.ValueChanged += new System.EventHandler(this.tmkembali_ValueChanged);
+            // 
+            // txtbiaya
+            // 
+            this.txtbiaya.Location = new System.Drawing.Point(217, 194);
+            this.txtbiaya.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbiaya.Name = "txtbiaya";
+            this.txtbiaya.ReadOnly = true;
+            this.txtbiaya.Size = new System.Drawing.Size(203, 22);
+            this.txtbiaya.TabIndex = 14;
+            this.txtbiaya.TextChanged += new System.EventHandler(this.Txtbiaya_TextChanged);
+            // 
+            // txtdurasi
+            // 
+            this.txtdurasi.Location = new System.Drawing.Point(617, 196);
+            this.txtdurasi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtdurasi.Name = "txtdurasi";
+            this.txtdurasi.ReadOnly = true;
+            this.txtdurasi.Size = new System.Drawing.Size(99, 22);
+            this.txtdurasi.TabIndex = 15;
+            this.txtdurasi.TextChanged += new System.EventHandler(this.Txtdurasi_TextChanged);
+            // 
+            // txtbayar
+            // 
+            this.txtbayar.Location = new System.Drawing.Point(387, 298);
+            this.txtbayar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbayar.Name = "txtbayar";
+            this.txtbayar.ReadOnly = true;
+            this.txtbayar.Size = new System.Drawing.Size(132, 22);
+            this.txtbayar.TabIndex = 16;
+            // 
+            // btnhitung
+            // 
+            this.btnhitung.Location = new System.Drawing.Point(573, 242);
+            this.btnhitung.Margin = new System.Windows.Forms.Padding(4);
+            this.btnhitung.Name = "btnhitung";
+            this.btnhitung.Size = new System.Drawing.Size(100, 28);
+            this.btnhitung.TabIndex = 5;
+            this.btnhitung.Text = "Hitung";
+            this.btnhitung.UseVisualStyleBackColor = true;
+            this.btnhitung.Click += new System.EventHandler(this.btnhitung_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(465, 242);
+            this.reset.Margin = new System.Windows.Forms.Padding(4);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(100, 28);
+            this.reset.TabIndex = 17;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 373);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.btnhitung);
+            this.Controls.Add(this.txtbayar);
+            this.Controls.Add(this.txtdurasi);
+            this.Controls.Add(this.txtbiaya);
+            this.Controls.Add(this.tmkembali);
+            this.Controls.Add(this.twkembali);
+            this.Controls.Add(this.tmpinjam);
+            this.Controls.Add(this.twpinjam);
+            this.Controls.Add(this.cbmobil);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Rental App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker twpinjam;
+        private System.Windows.Forms.DateTimePicker tmpinjam;
+        private System.Windows.Forms.DateTimePicker twkembali;
+        private System.Windows.Forms.DateTimePicker tmkembali;
+        private System.Windows.Forms.TextBox txtbiaya;
+        private System.Windows.Forms.TextBox txtdurasi;
+        private System.Windows.Forms.TextBox txtbayar;
+        private System.Windows.Forms.Button btnhitung;
+        private System.Windows.Forms.ComboBox cbmobil;
+        private System.Windows.Forms.Button reset;
+    }
+}
---
+++ TAucup/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAucup
+{
+    public partial class Form1 : Form
+    {
+        class Data
+        {
+            int avanza;
+            int xenia;
+            int apv;
+            int innova;
+            int alphard;
+
+            public void setAvanza(int val)
+            {
+                avanza = val;
+            }
+            public void setXenia(int val)
+            {
+                xenia = val;
+            }
+            public void setApv(int val)
+            {
+                apv = val;
+            }
+            public void setInnova(int val)
+            {
+                innova = val;
+            }
+            public void setAlphard(int val)
+            {
+                alphard = val;
+            }
+
+            public string getAvanza()
+            {
+                return Convert.ToString(avanza);
+            }
+            public string getXenia()
+            {
+                return Convert.ToString(xenia);
+            }
+            public string getApv()
+            {
+                return Convert.ToString(apv);
+            }
+            public string getInnova()
+            {
+                return Convert.ToString(innova);
+            }
+            public string getAlphard()
+            {
+                return Convert.ToString(alphard);
+            }
+            public string hitungbiaya(long val1, short val2)
+            {
+                return Convert.ToString(val1 * val2);
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //variabel untuk proses
+        long biaya_per_jam;
+        short waktu;
+        String mobil;
+        DateTime tanggal_pinjam;
+        DateTime tanggal_kembali;
+
+        Data m = new Data();
+
+        private void cbmobil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            mobil = Convert.ToString(cbmobil.SelectedItem);
+
+            if (mobil == "Avanza")
+            {
+                m.setAvanza(40000);
+                txtbiaya.Text = m.getAvanza();
+            }
+            else if (mobil == "Xenia")
+            {
+                m.setXenia(40000);
+                txtbiaya.Text = m.getXenia();
+            }
+            else if (mobil == "APV")
+            {
+                m.setApv(50000);
+                txtbiaya.Text = m.getApv();
+            }
+            else if (mobil == "Innova")
+            {
+                m.setInnova(50000);
+                txtbiaya.Text = m.getInnova();
+            }
+            else if (mobil == "Alphard")
+            {
+                m.setAlphard(100000);
+                txtbiaya.Text = m.getAlphard();
+            }
+
+        }
+
+        private void tmpinjam_ValueChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnhitung_Click(object sender, EventArgs e)
+        {
+
+            if (Int64.TryParse(txtbiaya.Text, out biaya_per_jam) && Int16.TryParse(txtdurasi.Text, out waktu))
+            {
+                txtbayar.Text = "Rp. " + m.hitungbiaya(biaya_per_jam, waktu) + ",-";
+            }
+        }
+
+        private void tmkembali_ValueChanged(object sender, EventArgs e)
+        {
+
+            tanggal_pinjam = tmpinjam.Value.Date + twpinjam.Value.TimeOfDay;
+            tanggal_kembali = tmkembali.Value.Date + twkembali.Value.TimeOfDay;
+
+            
+            if (tanggal_kembali < tanggal_pinjam)
+            {
+                MessageBox.Show("Tanggal yang anda masukan salah");
+            }
+            else
+            {
+                TimeSpan temp = tanggal_kembali - tanggal_pinjam;
+                txtdurasi.Text = Convert.ToString(Math.Round(temp.TotalHours));
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            txtbiaya.Clear();
+            txtbayar.Clear();
+            txtdurasi.Clear();
+            cbmobil.SelectedIndex = -1;
+            biaya_per_jam = 0;
+            waktu = 0;
+        }
+
+        private void Txtdurasi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txtbiaya_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
